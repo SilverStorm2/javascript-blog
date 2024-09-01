b51623d11d69856cbb19479055615e7240a9ecc5
Gabriela Gugulska-Sierant <ggs.amsterdam@gmail.com>
2024-09-01 18:13:24 +0200

    Added event listener to log all .titles a elements in console, currently commented out.

---
+++ js/script.js
@@ -0,0 +1,8 @@
+'use strict'; //pokazuje błędy, np literówka w słowie counter
+
+/*
+document.getElementById('test-button').addEventListener('click', function(){
+    const links = document.querySelectorAll('.titles a');
+    console.log('links:', links);
+  });
+  */
