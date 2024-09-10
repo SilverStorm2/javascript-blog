a0f6f6e07ca0bf2e775cdc0b910f132b556e01b6
Gabriela Gugulska-Sierant <ggs.amsterdam@gmail.com>
2024-09-11 02:39:27 +0300

    feat: Implement dynamic generation of article title links
    - Removed contents of the title list dynamically.
    - Fetched all articles and stored them in a variable.
    - Created HTML for each article link and inserted it into the title list.
    - Bound click events to the generated article links.

--- js/script.js
+++ js/script.js
@@ -10,7 +10,7 @@ const titleClickHandler = function(event){
     activeLink.classList.remove('active');
   }
 
-  /* [IN PROGRESS] add class 'active' to the clicked link */
+  /* [DONE] add class 'active' to the clicked link */
   console.log('clickedElement:', clickedElement);
   clickedElement.classList.add('active');
 
@@ -32,37 +32,48 @@ const titleClickHandler = function(event){
   targetArticle.classList.add('active');
 }
 
-const links = document.querySelectorAll('.titles a');
-for(let link of links){
-  link.addEventListener('click', titleClickHandler);
-}
-
 const optArticleSelector = '.post',
   optTitleSelector = '.post-title',
   optTitleListSelector = '.titles';
 
-function generateTitleLinks(){
-
+function generateTitleLinks() {
   /* remove contents of titleList */
   const titleList = document.querySelector(optTitleListSelector);
   titleList.innerHTML = '';
 
-  /* for each article */
-  
+  /* find all the articles and save them to variable: articles */
+  const articles = document.querySelectorAll(optArticleSelector);
 
+  /* create a variable to store all the generated links */
+  let html = '';
+
+  for (let article of articles) {
     /* get the article id */
+    const articleId = article.getAttribute('id');
 
     /* find the title element */
-
-    /* get the title from the title element */
+    const articleTitle = article.querySelector(optTitleSelector).innerHTML;
 
     /* create HTML of the link */
+    const linkHTML = '<li><a href="#' + articleId + '"><span>' + articleTitle + '</span></a></li>';
 
-    /* insert link into titleList */
+    /* log the generated HTML */
+    console.log('Generated link HTML:', linkHTML);
 
+    /* insert link into html variable */
+    html += linkHTML;
+  }
+
+  /* insert all the links into titleList */
+  titleList.innerHTML = html;
+
+  /* bind click events to the links */
+  const links = document.querySelectorAll('.titles a');
+  
+  for (let link of links) {
+    link.addEventListener('click', titleClickHandler);
+  }
 }
 
+/* Call the function to generate title links */
 generateTitleLinks();
-
-
-  
